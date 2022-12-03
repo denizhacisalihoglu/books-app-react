64967b32f6b5b317558366ce5a1b8e6c04a2d260
Deniz Hacısalihoğlu <deniz.hacisalihoglu@tunnl.com>
2022-12-04 01:39:52 +0300

    Create search element in homepage

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,33 @@
+import react from "react";
+import Logo from "../../Assets/images/books-logo.svg";
+import SearchIcon from "../../Assets/images/search-icon.svg";
+
+const SearchBar = () => {
+  return (
+    <>
+      <div className="flex h-screen">
+        <div className="m-auto text-left w-5/12">
+          <h3>
+            <img src={Logo} alt="Logo" />
+          </h3>
+          <p className="text-md text-gray-300">
+            Which book are you looking for?
+          </p>
+          <div className="w-full mt-4 search--container">
+            <img
+              src={SearchIcon}
+              alt="Search Icon"
+              className="invert absolute ml-4 mt-3"
+            />
+            <input
+              type="text"
+              className="px-3 py-3 rounded-full w-full indent-9 bg-gray-800 text-gray-400 focus:text-gray-700 focus:bg-white focus:border-violet-400 focus:outline-none"
+              placeholder="Type a book name..."
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default SearchBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      gray: colors.gray,
+      violet: colors.violet,
+    },
   },
   plugins: [],
 }
